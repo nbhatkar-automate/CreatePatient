4a5b6b91ea522abf8eee46dcded3b0f539f5f080
nbhatkar-automate <nbhatkar@outcomemd.com>
2025-11-18 19:16:11 +0530

    Update PatientCreate.java

--- src/test/java/Staging/PatientCreate.java
+++ src/test/java/Staging/PatientCreate.java
@@ -33,18 +33,14 @@ public class PatientCreate {
 
         firstName = faker.name().firstName();
         lastName = faker.name().lastName();
-        email = String.format(
-                "%s.%s%d@%s",
-                firstName.toLowerCase(),
-                lastName.toLowerCase(),
-                faker.number().numberBetween(100, 999),
-                "testdomain.io"
-        );
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() +
+                faker.number().numberBetween(100, 999) + "@testdomain.io";
+
         phone = faker.phoneNumber().subscriberNumber(10);
 
-        int year = 1980 + new java.util.Random().nextInt(25);
-        int month = 1 + new java.util.Random().nextInt(12);
-        int day = 1 + new java.util.Random().nextInt(28);
+        int year = 1980 + faker.number().numberBetween(0, 25);
+        int month = faker.number().numberBetween(1, 12);
+        int day = faker.number().numberBetween(1, 28);
 
         LocalDate dob = LocalDate.of(year, month, day);
         dobFormatted = dob.format(formatter);
@@ -52,75 +48,65 @@ public class PatientCreate {
 
     /** Open Quick Add Patient form + fill basic fields */
     public void openAddPatientForm() {
-
-        // Search box
         WebElement addPatientInput = wait.until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//input[@placeholder='Search or Add Patient (name, cell, or email)']")));
         addPatientInput.click();
 
-        // Click quick add
         WebElement addNew = wait.until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//div[contains(text(), 'Quick Add New Patient')]")));
         addNew.click();
 
-        // Enter phone
         wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//*[@id=\"app-component\"]/div[1]/div[2]/div/div[2]/div[1]/div[1]/div/div[1]/input")))
                 .sendKeys(phone);
 
-        // Click "Fill Out Full Info"
         WebElement fillFullForm = wait.until(ExpectedConditions.elementToBeClickable(
                 By.cssSelector("#app-component > div.modal > div.modal-box-container > div > div.patient-add-form.mobile > div.patient-add-short > div:nth-child(2) > div.fill-out-text > div")));
         scrollIntoView(fillFullForm);
         fillFullForm.click();
 
-        // Enter first name
         driver.findElement(By.xpath("//*[@id=\"app-component\"]/div[1]/div[2]/div/div[2]/div[2]/div[1]/div/div[1]/input"))
                 .sendKeys(firstName);
 
-        // Enter last name
         driver.findElement(By.xpath("//*[@id=\"app-component\"]/div[1]/div[2]/div/div[2]/div[2]/div[1]/div/div[2]/input"))
                 .sendKeys(lastName);
 
-        // Enter email
         driver.findElement(By.xpath("//*[@id=\"app-component\"]/div[1]/div[2]/div/div[2]/div[2]/div[1]/div/div[3]/input"))
                 .sendKeys(email);
 
-        // DOB (flatpickr)
         WebElement dateInput = wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.cssSelector("input[placeholder='Date of birth']")));
 
         ((JavascriptExecutor) driver)
                 .executeScript("arguments[0]._flatpickr.setDate('" + dobFormatted + "', true);", dateInput);
 
-        sleep(800);
-
-        // Scroll modal into view
-        WebElement modalContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.cssSelector("#app-component > div.modal > div.modal-box-container")));
-        scrollIntoView(modalContainer);
+        sleep(500);
     }
 
     /** Select Sex */
- public void selectSex(String value) {
-    By sexDropdown = By.xpath("//label[contains(text(), 'Sex')]/following::div[contains(@class,'custom-select')][1]");
-    WaitUtils.waitForPresence(driver, sexDropdown);
-    WebElement dropdown = driver.findElement(sexDropdown);
+    @Test
+    public void selectSex() {
 
-    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", dropdown);
-    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", dropdown);
+        String value = "Male";     // final selected sex
 
-    By option = By.xpath("//div[@class='option' and normalize-space()='" + value + "']");
-    WaitUtils.waitForPresence(driver, option);
+        By drop = By.xpath("//label[contains(text(), 'Sex')]/following::div[contains(@class,'custom-select')][1]");
+        WaitUtils.waitForPresence(driver, drop);
 
-    WebElement sexOption = driver.findElement(option);
-    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", sexOption);
-    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", sexOption);
+        WebElement dropdown = driver.findElement(drop);
+        scrollIntoView(dropdown);
+        dropdown.click();
 
-    System.out.println("Sex selected: " + value);
-}
+        By option = By.xpath("//div[@class='option' and normalize-space()='" + value + "']");
+        WaitUtils.waitForPresence(driver, option);
 
-    /** Select Gender */
+        WebElement sexOption = driver.findElement(option);
+        scrollIntoView(sexOption);
+        sexOption.click();
+
+        System.out.println("Sex selected: " + value);
+    }
+
+    /** Select Gender Identity */
     @Test(dependsOnMethods = "selectSex")
     public void selectGender() {
 
@@ -175,31 +161,29 @@ public class PatientCreate {
 
         WebElement addBtn = wait.until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//*[@id=\"app-component\"]/div[1]/div[2]/div/div[2]/div[2]/div[2]/button")));
-
         scrollIntoView(addBtn);
         addBtn.click();
 
-        // Handle modal if appears
         try {
             WebElement yesButton = wait.until(ExpectedConditions.elementToBeClickable(
                     By.xpath("//button[contains(text(), 'Yes Proceed')]")));
             yesButton.click();
-            System.out.println("Invalid number modal appeared. Clicked 'Yes Proceed'.");
-        } catch (Exception e) {
-            System.out.println("No invalid number modal appeared.");
+            System.out.println("Invalid number modal appeared. Clicked YES.");
+        } catch (Exception ignored) {
+            System.out.println("No invalid number modal.");
         }
 
-        // Wait for loader to disappear
         wait.until(ExpectedConditions.invisibilityOfElementLocated(
                 By.xpath("//*[@id='main-contents']/div/div/div/div[4]/div/div[1]/div/div/div/div/img")));
 
-        System.out.println("Patient Added: " + firstName + " " + lastName + " | " + phone + " | " + email);
+        System.out.println("Patient Added Successfully → " +
+                firstName + " " + lastName + " | " + phone + " | " + email);
     }
 
     // Utility scroll
     private void scrollIntoView(WebElement element) {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        sleep(400);
+        sleep(300);
     }
 
     private void clickWithScroll(String xpath) {
