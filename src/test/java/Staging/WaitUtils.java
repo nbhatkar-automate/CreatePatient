537097863705ab6f0a409b1a2685c32dea5b4492
nbhatkar-automate <nbhatkar@outcomemd.com>
2025-11-18 18:50:55 +0530

    Update WaitUtils.java

--- src/test/java/Staging/WaitUtils.java
+++ src/test/java/Staging/WaitUtils.java
@@ -1,17 +1,28 @@
+package Staging;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class WaitUtils {
 
-    public static WebElement waitForPresence(WebDriver driver, By locator) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
-        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
-    }
-
     public static WebElement waitForVisible(WebDriver driver, By locator) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        return new WebDriverWait(driver, Duration.ofSeconds(30))
+                .until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
     public static WebElement waitForClickable(WebDriver driver, By locator) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
-        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+        return new WebDriverWait(driver, Duration.ofSeconds(30))
+                .until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public static void scrollIntoView(WebDriver driver, WebElement element) {
+        ((JavascriptExecutor) driver)
+                .executeScript("arguments[0].scrollIntoView(true);", element);
     }
 }
