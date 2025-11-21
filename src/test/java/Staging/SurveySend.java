4495d24ed01da05c2f7f798b5197065009a3051d
nbhatkar-automate <nbhatkar@outcomemd.com>
2025-11-21 11:15:16 +0530

    Create SurveySend.java

---
+++ src/test/java/Staging/SurveySend.java
@@ -0,0 +1,67 @@
+package Staging;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class SurveySend {
+
+	public static WebDriver driver;
+	public static WebDriverWait wait;
+	
+    @Test
+    public void AddToCart() throws InterruptedException {
+
+        // Reuse driver & wait after PatientManagementTab finishes
+        driver = PatientManagementTab.sharedDriver;
+        wait = PatientManagementTab.sharedWait;
+
+        WebElement addBtn = wait.until(
+        	    ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='main-contents']/div/div/div/div/div[5]/div[1]/span[1]/div[2]")));
+
+        	// Scroll + JS Click
+        	((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center'});", addBtn);
+        	Thread.sleep(1000);
+        	((JavascriptExecutor) driver).executeScript("arguments[0].click();", addBtn);
+        	
+        //click on new assessment button
+        wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id=\"main-contents\"]/div/div/div/div/div[4]/div[3]/div[1]"))).click();
+        Thread.sleep(2000);
+        //enter cpt name in textbox
+        wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id=\"main-contents\"]/div/div/div/div[4]/div/div[1]/div/div[2]/div/span[1]/input")))
+                .sendKeys("Anxiety");
+        // open assessment
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"main-contents\"]/div/div/div/div[4]/div/div[1]/div/div[5]/div[2]/div/div[1]/div/div[1]/h3"))).click();
+        Thread.sleep(2000);
+        // add to cart
+        WebElement cptselect = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"main-contents\"]/div/div/div/div[4]/div/div[1]/div/div[5]/div[2]/div/div[1]/div/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div[3]/span")));
+        // Scroll + JS Click
+        	((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center'});", cptselect);
+        	Thread.sleep(1000);
+        	((JavascriptExecutor) driver).executeScript("arguments[0].click();", cptselect);
+    }
+
+    @Test(dependsOnMethods = "AddToCart")
+    public void SendAssessment() throws InterruptedException {
+    	
+    	// Radio Button select - THis Device
+    	 WebElement radiobtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"main-contents\"]/div/div/div/div[4]/div/div[2]/div/div/div[1]/div/div/div[4]/div[1]")));
+    	 // Scroll + JS Click
+         	((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center'});", radiobtn);
+         	Thread.sleep(1000);
+         	((JavascriptExecutor) driver).executeScript("arguments[0].click();", radiobtn);
+       
+        //Send Now Button 
+        wait.until(ExpectedConditions
+                .elementToBeClickable(By.xpath("//*[@id=\"main-contents\"]/div/div/div/div[4]/div/div[2]/div/div/div[1]/div/div/div[5]/span/span")))
+                .click();
+
+        System.out.println("Assessment sent to 'This Device'");
+    }
+}
