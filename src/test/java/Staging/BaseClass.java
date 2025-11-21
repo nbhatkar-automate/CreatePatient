3acb414ec46071e5a92f64f9a434931425bd20cb
nbhatkar-automate <nbhatkar@outcomemd.com>
2025-11-21 13:32:43 +0530

    Update BaseClass.java

--- src/test/java/Staging/BaseClass.java
+++ src/test/java/Staging/BaseClass.java
@@ -1,18 +1,41 @@
+package Staging;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
-@BeforeSuite
-public void startDriver() {
+import java.time.Duration;
 
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--headless=new");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--disable-gpu");
-    options.addArguments("--window-size=1920,1080");
+public class BaseClass {
 
-    driver = new ChromeDriver(options);
-    wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    public static WebDriver driver;
+    public static WebDriverWait wait;
 
-    driver.manage().window().maximize();
-    System.out.println("===== DRIVER STARTED (BeforeSuite) =====");
+    @BeforeSuite
+    public void startDriver() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        driver.manage().window().maximize();
+        System.out.println("===== DRIVER STARTED (BeforeSuite) =====");
+    }
+
+    @AfterSuite
+    public void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            System.out.println("===== DRIVER CLOSED (AfterSuite) =====");
+        }
+    }
 }
