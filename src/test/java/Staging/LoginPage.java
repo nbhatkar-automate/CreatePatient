0aa8cc8c7800d2408b1b49989dd600684193e983
nbhatkar-automate <nbhatkar@outcomemd.com>
2025-11-21 15:43:08 +0530

    Update LoginPage.java

--- src/test/java/Staging/LoginPage.java
+++ src/test/java/Staging/LoginPage.java
@@ -1,27 +1,44 @@
 package Staging;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+import java.time.Duration;
 
 public class LoginPage extends BaseClass {
 
     @Test
     public void loginTest() {
 
-        // Driver & wait already initialized in BaseClass
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-search-engine-choice-screen");
+        options.addArguments("--start-maximized");
+        options.addArguments("--force-device-scale-factor=1");
+        options.addArguments("--enable-features=NetworkService,NetworkServiceInProcess");
+        options.addArguments("--whitelisted-ips=''");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-site-isolation-trials");
+
+        // Use BaseClass driver
+        driver = WebDriverSetup.getDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(120));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         driver.get("https://stagingportal.outcomemd.com/");
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//input[@placeholder='Email']")))
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Email']")))
                 .sendKeys("nbhatkar@outcomemd.com");
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.cssSelector("input[type='password']")))
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")))
                 .sendKeys("Staging@1234");
 
-        wait.until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//button[text()='Sign In']")))
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Sign In']")))
                 .click();
 
         System.out.println("Login successful");
